62868418c066ea4d7d3f7cc4ff612e835788fa6e
sgonzalez <sgonzalezoyuela@gmail.com>
2023-08-13 02:22:17 -0300

    updated build

--- all.sh
+++ all.sh
@@ -8,9 +8,6 @@ for script_file in "$script_folder"/*.sh; do
   flake_name=$(basename "$script_file" .sh)
   echo "Building flake for $flake_name..."
   nix build ".#$flake_name"
-  echo "Installing flake for $flake_name..."
-  nix profile install --profile "/nix/var/nix/profiles/per-user/$USER/$flake_name" ".#$flake_name"
-  #nix profile install ".#$flake_name"
 done
 
 echo "All flakes have been built successfully!"
